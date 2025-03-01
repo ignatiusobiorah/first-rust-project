a6d59559e4dba76617bb3dbcd04375ee34948ae0
unknown <iobiorah23@gmail.com>
2025-03-01 18:11:16 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    // println!("Hello, Rust!");
+
+    //string
+    // let word: &str = "this guy";
+    // println!("word: {}", word);
+
+    //integer
+    // let numbers: [i32; 5] = [1, 2, 3, 4, 5];
+    // println!("Number array: {:?}", numbers);
+
+    //array
+    // let fruits: [&str; 3] = ["apple", "banana", "orange"];
+    // println!("This is my favourite fruit: {:?}", fruits[0]);
+    // println!("I eat this frequently: {}", fruits[1]);
+    // println!("I only like this fruit as a juice: {}", fruits[2]);
+
+    //mutubale string
+    // let mut name: String = String::from("Pope");
+    // println!("Name: {}", name);
+    // name.push_str("Francis");
+    // println!("Full Name: {}", name);
+
+    //variable
+    let a: &str ="Rust";
+    println!("The value of a is: {}", a);
+    // a = "crab";
+    // println!("The value of a is now: {}", a);
+}
